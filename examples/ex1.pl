2a0057f87aa0f6c88fe3d5fca12b75fb8316e5a2
John Lapeyre <lapeyre@physics.arizona.edu>
2011-12-29 21:19:52 +0000

    Import of JLAPEYRE/PDL-Fit-Levmar-0.0089 from CPAN.
    gitpan-cpan-distribution: PDL-Fit-Levmar
    gitpan-cpan-version:      0.0089
    gitpan-cpan-path:         JLAPEYRE/PDL-Fit-Levmar-0.0089.tar.gz
    gitpan-cpan-author:       JLAPEYRE
    gitpan-cpan-maturity:     released

---
+++ examples/ex1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use PDL;
+use PDL::Fit::Levmar;
+
+$n = 100;
+$t = 10 * (sequence($n)/$n -1/2);
+$x = 3 * exp(-$t*$t * .3  );
+$p = pdl [ 1, 1 ]; # initial  parameter guesses
+
+$h = levmar($p,$x,$t, FUNC =>
+                '   function
+                     x = p0 * exp( -t*t * p1);
+           ');
+print levmar_report($h);
